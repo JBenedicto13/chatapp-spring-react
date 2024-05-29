28d2aa2247cff61d89a9c0bb3e029137a2c86db2
JBenedicto13 <jbenedicto20.jbb@gmail.com>
2024-05-29 09:50:27 +0800

    Change setup of ipaddress for server side and update readme

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # chatapp-spring-react
 
 Note:
--Change the ip address under utils/config.js
--'npm run host' to expose the server to local network
+
+- Change the ip address under client/src/utils/config.js and server/source/main/resources/application.properties
+- 'npm run host' to expose the server to local network
--- server/src/main/java/com/websocket/chat/config/WebSocketConfig.java
+++ server/src/main/java/com/websocket/chat/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.websocket.chat.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,10 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${host-ip-address}")
+    private String hostIpAddress;
+
     @Override
     public void registerStompEndpoints(@SuppressWarnings("null") StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-        .setAllowedOrigins("http://192.168.40.141:5173", "http://localhost:5173")
+        .setAllowedOrigins("http://"+hostIpAddress+":5173", "http://localhost:5173")
         .withSockJS();
     }
 
